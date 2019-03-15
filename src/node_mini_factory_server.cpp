bd709ab8eef5905ebf5a682c59d8879459cb81f7
longbowliu <llb_feng@163.com>
2019-03-15 09:42:55 +0800

    steps2to8

---
+++ src/node_mini_factory_server.cpp
@@ -0,0 +1,107 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "mini_multi_agent/task_srv.h"
+#include "mini_multi_agent/rob_status.h"
+
+class TaskDespatcher {
+public:
+	TaskDespatcher();
+	~TaskDespatcher();
+	bool task1_processer(int seq);
+	bool task2_processer(int seq);
+//	bool task2_processer(int seq);
+	void messageCallback(const mini_multi_agent::rob_status::ConstPtr& msg);
+private:
+	ros::NodeHandle nh;
+	ros::Subscriber listener;
+	ros::ServiceClient client1;
+	ros::ServiceClient client2;
+
+};
+TaskDespatcher::TaskDespatcher() {
+	listener = nh.subscribe("agent_feedback", 10,
+			&TaskDespatcher::messageCallback, this);
+	client1 = nh.serviceClient<mini_multi_agent::task_srv>("agent_task_1");
+	client2 = nh.serviceClient<mini_multi_agent::task_srv>("agent_task_2");
+}
+TaskDespatcher::~TaskDespatcher() {
+
+}
+
+void TaskDespatcher::messageCallback(
+		const mini_multi_agent::rob_status::ConstPtr& msg) {
+//	ROS_INFO("robot[%d] : [%s]", msg->robot_id, msg->status.c_str());
+}
+
+bool TaskDespatcher::task1_processer(int seq) {
+	mini_multi_agent::task_srv srv1;
+	srv1.request.seq = seq;
+	if (client1.call(srv1)) {
+//		ROS_INFO("status: %s", srv1.response.status.c_str());
+	} else {
+		ROS_ERROR("SORRY , robot 1 task id [%d] FAILED!",seq);
+		return false;
+	}
+	return true;
+
+}
+
+bool TaskDespatcher::task2_processer(int seq) {
+	mini_multi_agent::task_srv srv1;
+	srv1.request.seq = seq;
+	if (client2.call(srv1)) {
+//		ROS_INFO("status: %s", srv1.response.status.c_str());
+	} else {
+		ROS_ERROR("SORRY , robot 2 task id [%d] FAILED!",seq);
+		return false;
+	}
+	return true;
+
+}
+
+
+int main(int argc, char **argv) {
+	ros::init(argc, argv, "node_mini_factory_server");
+	TaskDespatcher td;
+	ros::Rate loop_rate(10);
+	int limit_rob1 =0;   // make task servers in "ready" status and keep it 5 seconds
+	int count_rob1 =0;
+	int count_rob2 =0;
+	int limit_rob2 =0;
+	while ( ros::ok() && (limit_rob1 <=6 || limit_rob2<=6) ) {
+		count_rob1 ++;
+		count_rob2 ++;
+		if(count_rob1 >50 && limit_rob1<=6){
+			if(limit_rob1>1){
+				ROS_INFO("Robot 1 - task id : [%d] DONE !" , limit_rob1-1 );
+			}
+			if(limit_rob1<=5){
+				td.task1_processer(limit_rob1 );
+			}
+			limit_rob1++;
+			count_rob1 = 0;
+		}
+//		if(limit_rob1==6){
+//			ROS_INFO("Robot 1 - task id : [%d] DONE !" , limit_rob1-1 );
+//		}
+		if(count_rob2 >30 && limit_rob2<=6 ){
+			if(limit_rob2>1){
+				ROS_INFO("Robot 2 - task id : [%d] DONE !" , limit_rob2-1 );
+			}
+			if(limit_rob2<=5){
+				td.task2_processer(limit_rob2);
+			}
+			limit_rob2++;
+			count_rob2 = 0;
+		}
+//		if(limit_rob2==6){
+//			ROS_INFO("Robot 2 - task id : [%d] DONE !" , limit_rob2-1 );
+//		}
+		loop_rate.sleep();
+		ros::spinOnce();
+	}
+	ROS_INFO("Well Down ,robots! all tasks finised. ");
+	ros::shutdown();
+
+	return 0;
+}
---
+++ src/node_robot_1.cpp
@@ -0,0 +1,102 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "mini_multi_agent/task_srv.h"
+#include "mini_multi_agent/rob_status.h"
+using namespace std;
+
+#define time_precision  0.1; // seconds
+
+
+
+class Example {
+public:
+	Example(int seconds);
+	~Example();
+private:
+	ros::NodeHandle nh;
+	ros::ServiceClient client;
+	ros::Subscriber listener;
+	ros::Publisher pub;
+	int time_counter_limit;
+	int stas ;
+	int time_counter;
+    int seconds;
+	std_msgs::String msg;
+	std::stringstream ss;
+
+	bool add(mini_multi_agent::task_srv::Request &req,
+			mini_multi_agent::task_srv::Response &res);
+	void time_callback(const ros::TimerEvent&);
+
+};
+
+bool Example::add(mini_multi_agent::task_srv::Request &req,
+		mini_multi_agent::task_srv::Response &res) {
+
+	if (req.seq==0){
+		ROS_INFO("Robot 1 , just have a rest , we will going to work in 5 seconds . good luck!");
+		return true;
+	}
+	stas = 1;
+	if(stas==0)
+		res.status = "ready";
+	else
+		res.status = "executing";
+
+//	res.status = req.A + req.B + req.C;
+//	ROS_INFO("request: A=%d, B=%d C=%d", (int )req.A, (int )req.B, (int )req.C);
+	ROS_INFO("Robot 1 - task id : [%d] comming , status change to [%s]" , req.seq, res.status.c_str());
+	return true;
+}
+
+//implementation of construct function.
+Example::Example(int seconds):seconds(seconds) {
+	stas = 0;
+	time_counter = 0;
+	time_counter_limit = (int) seconds/time_precision;
+	ros::ServiceServer service = nh.advertiseService("agent_task_1" ,&Example::add,this);
+	ros::Timer  robot_timer = nh.createTimer(ros::Duration(0.1) ,&Example::time_callback,this);
+	pub = nh.advertise<mini_multi_agent::rob_status>("agent_feedback", 5);
+	ros::Rate loop_rate(10);
+		while (ros::ok()) {
+				loop_rate.sleep();
+				ros::spinOnce();
+			}
+}
+Example::~Example(){
+
+}
+
+//implementation of one of the Example class memeber.
+
+
+void Example::time_callback(const ros::TimerEvent&)
+{
+	if(stas == 1){
+		time_counter++;
+		if(time_counter >= time_counter_limit){
+			stas = 0;
+			time_counter = 0;
+		}
+	}
+	else{
+		time_counter = 0;
+	}
+
+	mini_multi_agent::rob_status msg;
+//	feedback_msg.header.stamp = ros::Time::now();
+	msg.robot_id = 1;
+	if(stas==0)
+		msg.status = "ready";
+	else
+		msg.status = "executing";
+
+	pub.publish(msg);
+//	ROS_INFO("status : [%d]", stas);
+}
+
+int main(int argc, char **argv) {
+	ros::init(argc, argv, "node_robot_1");
+	Example exp(5);
+	return 0;
+}
